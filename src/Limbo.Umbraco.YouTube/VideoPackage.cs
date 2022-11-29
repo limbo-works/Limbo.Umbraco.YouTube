39987295208b33ee5b3727bd67d16831cd1410d4
Anders Bjerner <abjerner@skybrud.dk>
2022-11-29 20:17:53 +0100

    Updated the package URLs a bit

--- src/Limbo.Umbraco.YouTube/VideoPackage.cs
+++ src/Limbo.Umbraco.YouTube/VideoPackage.cs
@@ -47,7 +47,7 @@ namespace Limbo.Umbraco.YouTube {
         /// <summary>
         /// Gets the URL of the documentation for this package.
         /// </summary>
-        public const string DocumentationUrl = "https://github.com/limbo-works/Limbo.Umbraco.YouTube#documentation";
+        public const string DocumentationUrl = "https://packages.limbo.works/limbo.umbraco.youtube/v2/docs/";
 
 
     }
