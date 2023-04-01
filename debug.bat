1e6a8f385adeef22f745f1b2719b170effa072c5
Anders Bjerner <abjerner@skybrud.dk>
2023-04-01 19:17:27 +0200

    Delete package files from new "docs" branch

--- debug.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-dotnet build src/Limbo.Umbraco.YouTube --configuration Debug /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=c:\nuget
--- release.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-dotnet build src/Limbo.Umbraco.YouTube --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
